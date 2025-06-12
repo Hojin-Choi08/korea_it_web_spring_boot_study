a05dfbf57b5993e420d99ce0aa5b63f7ff05ff82
Hojin Choi <merro3043@gmail.com>
2025-06-12 21:35:08 +0900

    Second Commit

--- src/main/java/com/koreait/spring_boot_study/Controller/PostController.java
+++ src/main/java/com/koreait/spring_boot_study/Controller/PostController.java
@@ -1,6 +1,7 @@
 package com.koreait.spring_boot_study.Controller;
 
 import com.koreait.spring_boot_study.Service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,11 +9,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/post")
 public class PostController {
-    private final PostService postService;
 
-    public PostController(PostService postService) {
-        this.postService = postService;
-    }
+    //Autowired
+    @Autowired //필요한 객체를 자동으로 주입해주는 어노테이션
+    private PostService postService;
+    //PostService가 주입되기전 시점에서 사용하게 되면 NPE(Null Point Exception)가 발생할 수도 있다.
+    //예를 들어서, 생성자에서 바로 쓴다거나 아니면 서비스, 레포지토리 어노테이션을 안 붙였거나
+//    private final PostService postService;
+
+    //...
+
+//    public PostController(PostService postService) {
+//        postService.getPost();
+//    }
+    //생성자 방식이 더 권장된다 => 명시적이고 명확
+    //final이 있기에 불변을 보장
+    //생성자로 주입하면 객체가 생성될 때 필수로 의존성을 받아야 한다
+    //그러면 이후에 그 의존성을 바꿀 수 없어서 안정적
+    //애초에 객체 생성이 되기도 전에 생성자를 통해 주입이 완료됨, 생성 전부터 준비가 완료 됨
 
     @GetMapping("/get")
     public String getPost() {
