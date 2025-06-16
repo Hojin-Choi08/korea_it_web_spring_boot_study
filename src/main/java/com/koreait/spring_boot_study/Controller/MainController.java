f1a2167bc3b9b99bdd5bd4dadbf78bb29b0be168
Hojin Choi <merro3043@gmail.com>
2025-06-16 19:04:35 +0900

    Third Commit

--- src/main/java/com/koreait/spring_boot_study/Controller/MainController.java
+++ src/main/java/com/koreait/spring_boot_study/Controller/MainController.java
@@ -3,7 +3,6 @@ package com.koreait.spring_boot_study.Controller;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
